89e50c3c0aa4aed0c0e7d4ac661fa768c67e914b
rohitmehta18 <rmehta1836@gmail.com>
2025-03-17 11:54:20 +0530

    done

---
+++ my-react-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h1>Rohit Mehta</h1>
+        <p>Email: rohitmehta@example.com</p>
+        <p>Phone: +91 9876543210</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
